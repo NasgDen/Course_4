eac56196fe408474b1274ac0689ec37d59a448f2
Denis Safonov <denissafonov_85@mail.ru>
2025-07-01 20:06:31 +0500

    Добавлен геттер и сеттер для атрибута price

--- src/product.py
+++ src/product.py
@@ -1,13 +1,13 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     # Метод принимает на вход параметры товара в словаре и возвращать созданный объект класса Product
@@ -17,3 +17,15 @@ class Product:
                    description=product["description"],
                    price=product["price"],
                    quantity=product["quantity"])
+
+    # Геттер для атрибута __price
+    @property
+    def price(self):
+        return self.__price
+
+    # Сеттер для атрибута __price
+    @price.setter
+    def price(self, price):
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        self.__price = price
